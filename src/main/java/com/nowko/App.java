09ed9e27688940869ed61662e109aff82238d522
nowko <magoga2896@gmail.com>
2026-08-25 12:53:17 +0200

    init

---
+++ src/main/java/com/nowko/App.java
@@ -0,0 +1,15 @@
+package com.nowko;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JavaPlugin
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
